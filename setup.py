b7bcb13457eba34dda23a62d9f88de03ee44521e
Oleksandr Glushchenko <contact@fluder.co>
2014-07-19 16:11:06 +0300

    preview fix for new posts at new page

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except:
     long_description = u"Blog generator for hackers."
 
 setup(
-    name='micropress',
-    version='0.2.2',
+    name='Micropress',
+    version='0.2.3',
     author='Oleksandr Glushchenko',
     author_email='contact@fluder.co',
     url='https://github.com/glushchenko/micropress',
