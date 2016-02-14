68f4f92676004b9a0461f886135a546eacbcef1d
Oleksandr Glushchenko <contact@fluder.co>
2016-02-14 14:29:59 +0200

    S3 watcher fix + init meta fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except:
 
 setup(
     name='micropress',
-    version='0.2.15',
+    version='0.2.16',
     author='Oleksandr Glushchenko',
     author_email='contact@fluder.co',
     url='https://github.com/glushchenko/micropress',
